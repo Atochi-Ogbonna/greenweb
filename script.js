337b52a2cdabac4a82b2dad8d7384172e2653798
Atochi Ogbonna <atochipraise87@gmail.com>
2023-12-15 18:19:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let signupBtn = document.getElementById("SignupBtn");
+let signinBtn = document.getElementById("SigninBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+
+
+signinBtn.onclick = function(){
+    nameField.style.maxHeight = "0"; 
+}
+
+
+
