978e9e0e711a8ab8bc7f8e935cb6f8692e112b6e
Atochi Ogbonna <atochipraise87@gmail.com>
2023-12-22 09:52:11 +0100

    Add files via upload

---
+++ GreenWeb sso/script.js
@@ -0,0 +1,20 @@
+// const optionMenu = document.querySelector(".select-menu"),
+
+// selectBtn = optionMenu.querySelector(".select-btn"),
+// options = optionMenu.querySelectorAll(".option"),
+// sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+// options.forEach(option =>{
+//     option.addEventListener("click", ()=>{
+//         let selectedOption = option.querySelector(".option-text").innerText;
+//         sBtn_text.innerText = selectedOption;
+//         console.log(selectedOption)
+//     })
+    
+// })
+
+
+
+
+
+
